8d8682f7051f6fd4b63fceeea48331e765790acd
qyn <qyn@example.com>
2025-09-16 09:44:17 +0700

    Tambah komponen Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import Profil from "./profil";
+import Login from "./login";
+
+function App() {
+  return (
+    <div>
+      <h1>Halo Dunia</h1>
+      <Login />
+    </div>
+  );
+}
+
+
 
 export default function App() {
   return (
