a77abe55e4420c87856e7328d8f8cd47da4d9e20
qyn <qyn@example.com>
2025-09-16 11:10:03 +0700

    Update dashboard.jsx

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
   return (
     <div>
-      <h1>Halaman Dashboard</h1>
-      <p>Selamat datang di dashboard React!</p>
+      <h1>Dashboard</h1>
+      <nav>
+        <ul>
+          <li><Link to="/profil">Profil</Link></li>
+          <li><Link to="/">Dashboard</Link></li>
+          <li><Link to="/login">Logout</Link></li>
+        </ul>
+      </nav>
     </div>
   );
 }
